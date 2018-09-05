94248c144643d40b929383679d2e2c8ad0776a2b
SilverW0o0W <silver.codingcat@gmail.com>
2018-09-05 20:41:02 +0800

    Update the fuck code, fix tomorrow

--- src/md_html.py
+++ src/md_html.py
@@ -1,20 +1,42 @@
 # coding=utf-8
 import markdown
-import pyh
-import pdfkit
+# import pdfkit
+from xml.dom.minidom import Document
+import HTMLParser
+
+# import cgi
 
 with open('resume.md', 'r') as f:
     text = f.read()
 
 text = text.decode('utf-8')
-html = markdown.markdown(text)
-html = html.encode('utf-8')
+text_html = markdown.markdown(text)
+text_html = text_html.encode('utf-8')
+# text_html = HTMLParser.HTMLParser().unescape(text_html)
 
-page = pyh.PyH()
-page.addCSS('Github.css', '1.css', '2.css')
-setattr(pyh.thisModule, 'article', pyh.TagFactory('article'))
-page << pyh.article(html, cl='markdown-body entry-content')
-page.printOut('test.html')
+doc = Document()
+html = doc.createElement("html")
+doc.appendChild(html)
+head = doc.createElement("head")
+
+for css_name in ['1.css', '2.css', 'Github.css']:
+    link = doc.createElement("link")
+    link.setAttribute('rel', 'stylesheet')
+    link.setAttribute('type', r'text/css')
+    link.setAttribute('href', css_name)
+    head.appendChild(link)
+html.appendChild(head)
+
+body = doc.createElement("body")
+article = doc.createElement("article")
+article.data = text_html
+content = doc.createTextNode(text_html)
+article.appendChild(content)
+body.appendChild(article)
+html.appendChild(body)
+
+with open('test.html', 'w') as f:
+    f.write(doc.toprettyxml())
 
 options = {
     'margin-top': '0.75in',
