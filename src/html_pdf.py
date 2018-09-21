66286a6ab158caa4d57b9960c29a6cbd75d4a30c
Silver <silver.codingcat@gmail.com>
2018-09-21 23:54:22 +0800

    Fix linux bug

--- src/html_pdf.py
+++ src/html_pdf.py
@@ -8,9 +8,9 @@ from utils import get_output_name
 
 def html_to_pdf(file_name, output_name=None):
     output_name = output_name if output_name else get_output_name(file_name, 'pdf')
-    configuration = pdfkit.configuration(wkhtmltopdf=Config["WKH2P_PATH"])
+    wk_path = Config.get('WKH2P_PATH', '')
+    configuration = pdfkit.configuration(wkhtmltopdf=wk_path) if wk_path else None
     pdfkit.from_file(file_name, output_name,
                      configuration=configuration,
-                     options=Config["WKH2P_OPTION"]
-                     )
+                     options=Config["WKH2P_OPTION"])
     return output_name
